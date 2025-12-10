bf26cf3636d01a3a6555fd8e18b67ed71e5a5398
Euael <euaelmeko@gmail.com>
2025-12-10 17:44:57 +0300

    fix: Update React and React DOM to latest versions and improve Jest setup for cleanup

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,26 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import { cleanup } from '@testing-library/react'
 
 // Mock react-markdown to avoid ES module issues in Jest
 jest.mock('react-markdown', () => {
   return function ReactMarkdown({ children }) {
     return children;
   };
+});
+
+// Clean up after each test for React Testing Library
+afterEach(() => {
+  cleanup();
+});
+
+// Clean up database connections after all tests
+afterAll(async () => {
+  // Dynamically import to avoid issues when DATABASE_URL is not set
+  try {
+    const { closePool } = await import('./src/lib/db');
+    await closePool();
+  } catch (error) {
+    // Ignore errors if db module can't be loaded
+  }
 });
